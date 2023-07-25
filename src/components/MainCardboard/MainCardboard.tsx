f2e29d7765f5597814d6d37ae9fa89895b1720ae
Kingsley Yong <kingsleyyong0208@gmail.com>
2023-07-26 07:53:36 +0800

    feat - implemented children placement, css adjustment

--- src/components/MainCardboard/MainCardboard.tsx
+++ src/components/MainCardboard/MainCardboard.tsx
@@ -1,10 +1,11 @@
-import { Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
 import {
 	Coordinate,
 	DashBoardMainCard,
 	DashboardCardElement,
 } from '../../types/globalTypes';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import EastIcon from '@mui/icons-material/East';
 import style from './MainCardboard.module.sass';
 import { Fragment } from 'react';
 
@@ -17,86 +18,113 @@ const MainCardboard = ({ cardInformation }: MainCardboardProp) => {
 
 	return (
 		<div className={style.cardBox}>
-			<div className={style.mockInfoDiv}>
-				{caption && (
-					<Typography variant={'caption'} color={style.gray400}>
-						{caption}
-					</Typography>
-				)}
-				<Typography
-					variant={'body1'}
-					mr={1}
-					mt={1}
-					mb={1}
-					color={`${
-						image &&
-						image.coordinate === Coordinate.FULL_SCREEN &&
-						style.white
-					}`}
-				>
-					{title}
-				</Typography>
-				<div className={style.descriptionBox}>
-					{description.greenText !== undefined &&
-						description.greenText === false && (
-							<Fragment>
-								<CheckCircleIcon
-									sx={{
-										color: style.green400,
-										fontSize: '1rem',
-										mr: '0.5%',
-									}}
-								/>{' '}
-							</Fragment>
-						)}
+			{Array.isArray(children) && children[0]}
 
-					{description.highlightText && (
-						<Typography
-							variant={'caption'}
-							color={description.greenText ? style.green400 : ''}
-							mr={'0.5%'}
-						>
-							{description.highlightText}{' '}
+			<div className={style.horizontalFlex}>
+				<div className={style.mockInfoDiv}>
+					{caption && (
+						<Typography variant={'caption'} color={style.gray400}>
+							{caption}
 						</Typography>
 					)}
-
 					<Typography
-						variant={'caption'}
-						fontWeight={'fontWeightLight'}
-						color={
-							image?.coordinate === Coordinate.FULL_SCREEN
-								? style.white
-								: style.gray400
-						}
+						variant={'body1'}
+						mr={1}
+						mt={1}
+						mb={1}
+						color={`${
+							image &&
+							image.coordinate === Coordinate.FULL_SCREEN &&
+							style.white
+						}`}
 					>
-						{description.text.split('\n').map((line) => (
-							<div key={`${line}`}>{line}</div>
-						))}
+						{title}
 					</Typography>
+					<div className={style.descriptionBox}>
+						{description.greenText !== undefined &&
+							description.greenText === false && (
+								<Fragment>
+									<CheckCircleIcon
+										sx={{
+											color: style.green400,
+											fontSize: '1rem',
+											mr: '0.5%',
+										}}
+									/>{' '}
+								</Fragment>
+							)}
+
+						{description.highlightText && (
+							<Typography
+								variant={'caption'}
+								color={
+									description.greenText ? style.green400 : ''
+								}
+								mr={'0.5%'}
+							>
+								{description.highlightText}{' '}
+							</Typography>
+						)}
+
+						<Typography
+							variant={'caption'}
+							fontWeight={'fontWeightLight'}
+							color={
+								image?.coordinate === Coordinate.FULL_SCREEN
+									? style.white
+									: style.gray400
+							}
+						>
+							{description.text.split('\n').map((line) => (
+								<div key={`${line}`}>{line}</div>
+							))}
+						</Typography>
+					</div>
+
+					<Link underline={'none'} href={''}>
+						{readMoreOption && (
+							<Typography
+								variant={'caption'}
+								fontWeight={'fontWeightLight'}
+								color={
+									image &&
+									image.coordinate === Coordinate.FULL_SCREEN
+										? style.white
+										: style.gray700
+								}
+								className={style.readMoreDiv}
+								sx={{ marginTop: 'auto' }}
+							>
+								Read More
+								<EastIcon sx={{ fontSize: '1rem' }} />
+							</Typography>
+						)}
+					</Link>
 				</div>
+				{image && (
+					<Fragment>
+						<img
+							draggable={false}
+							src={image.path}
+							alt={image.path}
+							className={`${
+								image.coordinate === Coordinate.RIGHT &&
+								style.rightImage
+							} ${
+								image.coordinate === Coordinate.FULL_SCREEN &&
+								style.fullImage
+							}`}
+						/>
+						{image.coordinate === Coordinate.FULL_SCREEN && (
+							<div
+								className={`${style.gradientOverlay} ${style.fullImage}`}
+							/>
+						)}
+					</Fragment>
+				)}
 			</div>
 
-			{image && (
-				<Fragment>
-					<img
-						draggable={false}
-						src={image.path}
-						alt={image.path}
-						className={`${
-							image.coordinate === Coordinate.RIGHT &&
-							style.rightImage
-						} ${
-							image.coordinate === Coordinate.FULL_SCREEN &&
-							style.fullImage
-						}`}
-					/>
-					{image.coordinate === Coordinate.FULL_SCREEN && (
-						<div
-							className={`${style.gradientOverlay} ${style.fullImage}`}
-						/>
-					)}
-				</Fragment>
-			)}
+			{Array.isArray(children) ? children[0] : children}
 		</div>
 	);
 };
